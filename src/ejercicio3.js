b5d9b8a5245e51449037064d9db2b7787182c56d
EmelMayorga <emayorga@fwdcostarica.com>
2024-08-16 11:08:16 -0600

    ejercicio3

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,39 @@
 // Inserte el código aquí
 
+//1//
+function clonarArreglo() {
+    const arreglo = [1,2,3]
+    const nuevoArreglo = [... arreglo]
+    return nuevoArreglo
+
+}
+
+//2//
+function clonarObjeto() {
+    let objeto = {
+        nombre: "Maria",
+        edad: 20
+    }
+    const nuevoObjeto = {... objeto}
+    return nuevoObjeto
+}
+
+//3//
+const numeros = [1,2,3,4]
+function sumar(... numeros) {
+    return numeros.reduce((acumulador, valorActual)=>{
+        return acumulador + valorActual
+    }, 0)
+    
+}
+sumar(1,2,3,4)
+//4//
+
+function saludar(nombre2 = "Desconocido") {
+    return ("Hola", nombre2)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+}
 
 
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+export { clonarArreglo, clonarObjeto, sumar, saludar };                                                                                                  
